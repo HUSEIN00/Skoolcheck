837abb14f7e7704b619076f2fd195fdee29cd7e6
HUSEIN00 <halidovhusein@gmail.com>
2022-09-24 13:32:22 +0400

    dasdsa

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['HUS', 'Magomed', 'Adam', 'Israil', 'Ysup', 'Halidov']
+  const labels = ['HUS', 'Magomed', 'Adam', 'Israil', 'Ysup']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
